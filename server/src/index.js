11d84ae55d8bfe298725e06016cafdc6231d380f
debelopumento <imdiye@gmail.com>
2017-11-13 19:03:31 -0800

    refactoring and cleanups

--- server/src/index.js
+++ server/src/index.js
@@ -1,31 +1,12 @@
 import express from 'express'
-import React from 'react'
-import { renderToString } from 'react-dom/server'
-import Home from './client/components/Home'
-import Landing from './client/components/Landing'
+import renderer from './helpers/renderer'
 
 const app = express()
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-	const content = renderToString(<Home />)
-	const html = `
-		<html>
-			<head></head>
-			<body>
-				<div id="root">${content}</div>
-				<script src="bundle.js"></script>
-			</body>
-		</html>
-	`
-
-	res.send(html)
-})
-
-app.get('/landing', (req, res) => {
-	const content = renderToString(<Landing />)
-	res.send(content)
+	res.send(renderer())
 })
 
 app.listen(3000, () => {
