089a7895ba04121c165eaed3dda61fc7966be760
debelopumento <imdiye@gmail.com>
2017-11-14 15:09:21 -0800

    added async calls

--- server/src/helpers/renderer.js
+++ server/src/helpers/renderer.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 import { renderToString } from 'react-dom/server'
 import { StaticRouter } from 'react-router-dom'
 import Routes from '../client/Routes'
 
-export default req => {
+export default (req, store) => {
 	const content = renderToString(
-		<StaticRouter location={req.path} context={{}}>
-			<Routes />
-		</StaticRouter>,
+		<Provider store={store}>
+			<StaticRouter location={req.path} context={{}}>
+				<Routes />
+			</StaticRouter>
+		</Provider>,
 	)
 	return `
 		<html>
