fa57f2e9ae1b6128d58aaf8f99f106ca18aa1e75
debelopumento <imdiye@gmail.com>
2017-11-15 09:10:40 -0800

    test

--- src/client/pages/HomePage.js
+++ src/client/pages/HomePage.js
@@ -1,20 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
-	return (
-		<div>
-			<div>I'm the best home component</div>
-			<button
-				onClick={() => {
-					console.log('home button clicked')
-				}}
-			>
-				Press me!
-			</button>
-		</div>
-	)
-}
+  return (
+    <div className="center-align" style={{ marginTop: '200px' }}>
+      <h3>Welcome</h3>
+      <p>Check out these awesome features</p>
+    </div>
+  );
+};
 
 export default {
-	component: Home,
-}
+  component: Home
+};
--- src/client/pages/UsersListPage.js
+++ src/client/pages/UsersListPage.js
@@ -1,35 +1,48 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { fetchUsers } from '../actions'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchUsers } from '../actions';
+import { Helmet } from 'react-helmet';
 
 class UsersList extends Component {
-	componentDidMount() {
-		this.props.fetchUsers()
-	}
+  componentDidMount() {
+    this.props.fetchUsers();
+  }
 
-	renderUsers() {
-		return this.props.users.map(user => {
-			return <li key={user.id}>{user.name}</li>
-		})
-	}
+  renderUsers() {
+    return this.props.users.map(user => {
+      return <li key={user.id}>{user.name}</li>;
+    });
+  }
 
-	render() {
-		return (
-			<div>
-				Here's a big list of users.
-				<ul>{this.renderUsers()}</ul>
-			</div>
-		)
-	}
+  head() {
+    return (
+      <Helmet>
+        <title>{`${this.props.users.length} Users Loaded`}</title>
+        <meta property="og:title" content="Users App" />
+      </Helmet>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.head()}
+        Here's a big list of users:
+        <ul>{this.renderUsers()}</ul>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => ({
-	users: state.users,
-})
+function mapStateToProps(state) {
+  return { users: state.users };
+}
 
-const loadData = store => store.dispatch(fetchUsers())
+function loadData(store) {
+  return store.dispatch(fetchUsers());
+}
 
 export default {
-	loadData,
-	component: connect(mapStateToProps, { fetchUsers })(UsersList),
-}
+  loadData,
+  component: connect(mapStateToProps, { fetchUsers })(UsersList)
+};
